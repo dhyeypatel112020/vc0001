363d0c50f415fddb5a41fd94b7fe03a51f915f2f
dhyeypatel112020 <87629011+dhyeypatel112020@users.noreply.github.com>
2022-11-09 08:44:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+// Thisis peer server
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+// i need server to this application
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room",(roomId,userId,userName) => {
+        socket.join(roomId)
+    
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage",message,userName)
+    });
+    })
+});
+socket.on("createMessage",(message,userName)=>{
+    $(".message").append(`
+    <div class="message">
+    <b><i><span>${
+        userName==user?"me":userName
+    }</span></i></b>
+    </div>
+    `)
+})
+server.listen(3030);
